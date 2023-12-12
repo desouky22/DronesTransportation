ab7332b1066cff21accfdfebc9fc0d1e48bd0962
Omar Essam Desouky <omaressamdesouky2274@gmail.com>
2023-12-12 15:23:23 +0200

    FirstCommit(Project): initial commit

---
+++ src/main/java/com/elmenus/DronesTransportation/DronesTransportationApplication.java
@@ -0,0 +1,11 @@
+package com.elmenus.DronesTransportation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronesTransportationApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DronesTransportationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/elmenus/DronesTransportation/DronesTransportationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elmenus.DronesTransportation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesTransportationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
